dc825f6242df79a07ba149e273f6913bd7b07059
Shakhtarin Daniil <112755923+FrenzyFly@users.noreply.github.com>
2022-12-03 22:21:06 +0300

    Delete task12 directory

--- task12/SortStud.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.task12;
-import java.util.ArrayList;
-import java.util.Comparator;
-
-public class SortStud implements Comparator {
-    public int compare(Object o1, Object o2) {
-        return ((Student) o1).getScore() - ((Student) o2).getScore();
-    }
-}
